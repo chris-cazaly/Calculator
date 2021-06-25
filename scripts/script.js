3c35c71d317e36c4abd020cab3f0a620899e68dc
Chris Cazaly <81646471+chris-cazaly@users.noreply.github.com>
2021-06-25 06:48:14 +0100

    - remove placeholder text + bugfixes
    - Remove "secondary-text" placeholder text
    - Bugfix: If the sum finishes with an operator, the calculator cannot evaluate it
    - Bugfix: If you use the DEL button to delete an operator, none of the operator buttons will function until you press a number

--- scripts/script.js
+++ scripts/script.js
@@ -17,102 +17,96 @@ let previous_button = "";
 // ----- BUTTON EVENT LISTENERS ----- //
 
 // - number buttons - 
-for (let i=0; i<number_buttons.length; i++){
+for (let i = 0; i < number_buttons.length; i++) {
 
     let this_button = number_buttons[i];
 
-    this_button.addEventListener("click", function(){
-            primary_text.innerHTML += this_button.id;  
-            
-            previous_button = "number";             // < button tracking
+    this_button.addEventListener("click", function () {
+        primary_text.innerHTML += this_button.id;
+
+        previous_button = "number"; // < button tracking
     });
 }
 
 // - operator buttons - 
-for (let i=0; i<operator_buttons.length; i++){
+for (let i = 0; i < operator_buttons.length; i++) {
     let this_button = operator_buttons[i];
 
-    this_button.addEventListener("click", function(){
+    this_button.addEventListener("click", function () {
 
-            if (previous_button != "operator"){     // < prevent multiple operators
+        if (previous_button != "operator") { // < prevent multiple operators
 
-                let id = this_button.id;
-                let p_text = primary_text.innerHTML;
+            let id = this_button.id;
+            let p_text = primary_text.innerHTML;
 
-                let illegal_input = (p_text == "") 
-                                    && ( (id == "/") || (id == "*") || (id == "+") );
+            let illegal_input = (p_text == "") &&
+                ((id == "/") || (id == "*") || (id == "+"));
 
-                if (illegal_input){                 // < prevent / * + as first input
-                    // do nothing
-                }
-                else{
-                    primary_text.innerHTML += " " + id + " ";  
-            
-                    previous_button = "operator";   // < button tracking 
-                }
+            if (illegal_input) { // < prevent / * + as first input
+                // do nothing
+            } else {
+                primary_text.innerHTML += " " + id + " ";
 
-                
+                previous_button = "operator"; // < button tracking 
             }
 
-            
-            
+
+        }
+
+
+
     });
 }
 
 // - special operator buttons -
-for (let i=0; i<special_operator_buttons.length; i++){
+for (let i = 0; i < special_operator_buttons.length; i++) {
     let this_button = special_operator_buttons[i];
 
-    this_button.addEventListener("click", function(){
+    this_button.addEventListener("click", function () {
 
-        switch (this_button.id){
+        switch (this_button.id) {
 
-            case "CLEAR":        
-                                                   
+            case "CLEAR":
+
                 primary_text.innerHTML = "";
                 secondary_text.innerHTML = "";
                 previous_button = "";
-    
+
                 break;
-            
+
             case "DELETE":
 
                 // v determine where to end v
                 let del_amount = primary_text.innerHTML.length; // < default: don't delete
-                if (previous_button == "number") {
-                    del_amount = -1;                            // < number: delete 1
-                } 
-                else if (previous_button == "operator"){
-                    del_amount = -3;                            // < operator: delete 3
-                }
                 
-                // reassign previous number
-                // if prev_but == " " then set prev_but to operator
-                // else it's a number
-                // !!!!!!!!                  !!!!!!!!
-                // !!!!!!!! NEED TO COMPLETE !!!!!!!!
-                // !!!!!!!!                  !!!!!!!!
-                    
+                if (previous_button == "number") {
+                    del_amount = -1; // < number: delete 1
+                } else if (previous_button == "operator") {
+                    del_amount = -3; // < operator: delete 3
+                    previous_button = "number";
+                }
+
                 primary_text.innerHTML = primary_text.innerHTML.slice(0, del_amount);
 
                 break;
 
             case "EQUALS":
 
-                // CHECK THAT PREVIOUS INPUT IS A NUMBER
-                // - if not, then delete the last operator
+                if (previous_button == "operator") { // < remove trailing operator
+                    let del_amount = -3;
+                    primary_text.innerHTML = primary_text.innerHTML.slice(0, del_amount);
+                }
 
                 // EVALUATE
                 secondary_text.innerHTML = primary_text.innerHTML + " = " + eval(primary_text.innerHTML);
-                
+
                 primary_text.innerHTML = eval(primary_text.innerHTML);
 
-                
-                
-                break;
-                
-            
-        }
-    });  
-}
 
+
+                break;
+
+
+        }
+    });
+}
